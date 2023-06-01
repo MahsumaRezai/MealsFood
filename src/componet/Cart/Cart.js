9ef77560c2cd3059610ba06f5a4014f166958ca4
Masoumeh Rezai <you@example.com>
2023-06-02 02:05:42 +0430

    work on Modal section

--- src/componet/Cart/Cart.js
+++ src/componet/Cart/Cart.js
@@ -1,20 +1,23 @@
+import classes from './Cart.module.css';
+
+
 const Cart = (porps) => {
+    const cartItems = (<ul className={classes['cart-items ']}>{[
+        { id: "m1", name: "sushi", amount: 2, price: 23.65 }].map((item) => <li>{item.name}</li>)}
+    </ul>
+    );
     return (
         <div>
-            <div>
+            {cartItems}
+            <div className={classes.total}>
                 <span>Total Amount</span>
                 <span>35.76</span>
             </div>
-            <div>
-                <button>Close</button>
-                <button>Order</button>
+            <div className={classes.action}>
+                <button className={classes['button--all']}>Close</button>
+                <button className={classes.button}>Order</button>
 
             </div>
-
-
-
-
-
 
         </div>
     )
