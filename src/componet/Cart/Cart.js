5d3cdca9eef1f7d883af4799817c12c1e3a7978b
Masoumeh Rezai <you@example.com>
2023-06-02 01:46:34 +0430

    work on Cart section

---
+++ src/componet/Cart/Cart.js
@@ -0,0 +1,22 @@
+const Cart = (porps) => {
+    return (
+        <div>
+            <div>
+                <span>Total Amount</span>
+                <span>35.76</span>
+            </div>
+            <div>
+                <button>Close</button>
+                <button>Order</button>
+
+            </div>
+
+
+
+
+
+
+        </div>
+    )
+}
+export default Cart;
