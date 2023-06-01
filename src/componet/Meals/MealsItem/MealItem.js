2bf2c5d765153ab1f7864edaf8278e1403e83fff
Masoumeh Rezai <you@example.com>
2023-06-01 08:38:54 +0430

    work on input section

--- src/componet/Meals/MealsItem/MealItem.js
+++ src/componet/Meals/MealsItem/MealItem.js
@@ -1,3 +1,4 @@
+import MealsFrom from './MealsFrom';
 import classes from './MealsItme.module.css'
 const MealItem = (props) => {
     const price = `$${props.price.toFixed(2)}`;
@@ -10,6 +11,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
+                <MealsFrom / >
             </div>
         </li>
     );
--- src/componet/Meals/MealsItem/MealsFrom.js
+++ src/componet/Meals/MealsItem/MealsFrom.js
@@ -1,8 +1,7 @@
 import classes from './MealItemForm.module.css';
-
 const MealsFrom = (props) => {
     return <form className={classes.form} >
-        <input></input>
+        <input />
         <button>+ Add</button>
     </form>
 }
