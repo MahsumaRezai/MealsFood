18090de94e22bfddbd453383dd200872600fe8f9
Masoumeh Rezai <you@example.com>
2023-06-02 22:18:33 +0430

    work on usecaontext

--- src/componet/Layout/HearderCartButton.js
+++ src/componet/Layout/HearderCartButton.js
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../store/cart-context';
 const HearderCartButton = (props) => {
+    const cartCTX = useContext(CartContext);
+    const numberCartOfItme = cartCTX.items.length;
     return (
         <button className={classes.button} onClick={props.onClik}>
             <span>
